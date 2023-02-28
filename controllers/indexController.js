860d3599102604757d816af93ec385da90fc6483
wexsilon <wexsilon2001@protonmail.com>
2023-02-28 16:48:26 +0330

    install ipaddr.js and use it

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,21 +1,8 @@
 const requestIp = require('request-ip');
-const https = require('node:https');
+const ipaddr = require('ipaddr.js');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     let ip = requestIp.getClientIp(req);
-    if (ip === '::ffff:127.0.0.1') {
-        https.get('https://ip.42.pl/raw', resp => {
-            let data = [];
-            resp.on('data', chunk => {
-                data.push(chunk);
-            });
-            resp.on('end', () => {
-                ip = Buffer.concat(data).toString();
-                res.render('index', { title: 'Show IP', ip });
-            });
-        });
-    } else {
-        res.render('index', { title: 'Show IP', ip });    
-    }
-    
+    ip = ipaddr.process(ip).toString();
+    res.render('index', { title: 'Show IP', ip });  
 };
