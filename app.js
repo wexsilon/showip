d1255d945103d5b0ca26db9898826df80ab99821
wexsilon <wexsilon2001@protonmail.com>
2023-02-28 12:26:30 +0330

    edit app.js

--- app.js
+++ app.js
@@ -7,32 +7,13 @@ const morgan = require('morgan');
 const app = express();
 
 async function main() {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect('mongodb://0.0.0.0:27017/DBNAME');
     
     app.set('view engine', 'ejs');
     app.set('views', path.join(__dirname, 'views'));
     
     app.use(morgan('dev'));
     app.use(serveStatic(path.join(__dirname, 'public')));
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.use(cookieParser());
     
-    app.use(
-        expressSession(
-            {
-                secret: 'keyboard cat',
-                resave: false,
-                saveUninitialized: false,
-                store: MongoStore.create(
-                    {
-                        mongoUrl: 'mongodb://0.0.0.0:27017/DBNAME_SESSION'
-                    }
-                )
-            }
-        )
-    );
 
     app.listen(8000, () => console.log('Started Server On Port 8000'));
 }
