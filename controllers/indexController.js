c0b52e81c11b5f482af2e593c4180b94bb9a0d83
wexsilon <wexsilon2001@protonmail.com>
2023-02-28 15:20:53 +0330

    handle ip 127.0.0.1

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,21 @@
 const requestIp = require('request-ip');
+const https = require('node:https');
 
 module.exports = (req, res) => {
     let ip = requestIp.getClientIp(req);
-    if (ip.startsWith('::ffff:'))
-        ip = ip.substr(7);
-    res.render('index', { title: 'Show IP', ip });
+    if (ip === '::ffff:127.0.0.1') {
+        https.get('https://ip.42.pl/raw', resp => {
+            let data = [];
+            resp.on('data', chunk => {
+                data.push(chunk);
+            });
+            resp.on('end', () => {
+                ip = Buffer.concat(data).toString();
+                res.render('index', { title: 'Show IP', ip });
+            });
+        });
+    } else {
+        res.render('index', { title: 'Show IP', ip });    
+    }
+    
 };
